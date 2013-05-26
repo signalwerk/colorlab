3a5f0825cc964a7ff5343e0cc651a80d2f5a037d
glender <sh@signalwerk.ch>
2013-05-26 16:38:44 +0200

    MOD: remove LOG for deltaE

--- colorLab.js
+++ colorLab.js
@@ -160,8 +160,8 @@ var colorLab = (function(space, values){
             // http://www.ece.rochester.edu/~gsharma/ciede2000/dataNprograms/deltaE2000.m
             // http://en.wikipedia.org/wiki/Color_difference
 
-            console.log( this.L(), this.a(), this.b() );
-            console.log( newPoint.CIELAB.L(), newPoint.CIELAB.a(), newPoint.CIELAB.b() );
+            // console.log( this.L(), this.a(), this.b() );
+            // console.log( newPoint.CIELAB.L(), newPoint.CIELAB.a(), newPoint.CIELAB.b() );
 
             // weighting factors
             var k_L = 1;
@@ -187,26 +187,26 @@ var colorLab = (function(space, values){
             var C_avr = (Lab1.C + Lab2.C) / 2; // average of the two C
 
 
-            console.log(Lab1);
-            console.log(Lab2);
+            // console.log(Lab1);
+            // console.log(Lab2);
 
             // get G for the colors
             var G = 0.5 * (1 - Math.sqrt(Math.pow(C_avr, 7) / (Math.pow(C_avr, 7) + Math.pow(25, 7))));
-            console.log("G", G);
+            // console.log("G", G);
 
             // add for both colors the a'
             Lab1.a_1 = (1 + G) * Lab1.a;
             Lab2.a_1 = (1 + G) * Lab2.a;
 
-            console.log("Lab1.a_1", Lab1.a_1);
-            console.log("Lab2.a_1", Lab2.a_1);
+            // console.log("Lab1.a_1", Lab1.a_1);
+            // console.log("Lab2.a_1", Lab2.a_1);
 
             // add for both colors the C'
             Lab1.C_1 = Math.sqrt(Math.pow(Lab1.a_1, 2) + Math.pow(Lab1.b, 2));
             Lab2.C_1 = Math.sqrt(Math.pow(Lab2.a_1, 2) + Math.pow(Lab2.b, 2));
 
-            console.log("Lab1.C_1", Lab1.C_1);
-            console.log("Lab2.C_1", Lab2.C_1);
+            // console.log("Lab1.C_1", Lab1.C_1);
+            // console.log("Lab2.C_1", Lab2.C_1);
 
 
             // add h' for both colors
@@ -230,8 +230,8 @@ var colorLab = (function(space, values){
                 }
             }
 
-            console.log("Lab1.h",Lab1.h);
-            console.log("Lab2.h",Lab2.h);
+            // console.log("Lab1.h",Lab1.h);
+            // console.log("Lab2.h",Lab2.h);
 
 
 
@@ -254,17 +254,17 @@ var colorLab = (function(space, values){
             }
             var deltaH = 2 * Math.sqrt( Lab1.C_1 * Lab2.C_1 ) * Math.sin( root.helper.math.toRad( deltah / 2 ) );
 
-            console.log("deltah: ", deltah);
-            console.log("deltaH: ", deltaH);
+            // console.log("deltah: ", deltah);
+            // console.log("deltaH: ", deltaH);
 
             // the delta for lightness
             var deltaL = Lab1.L - Lab2.L;
-            console.log("deltaL", deltaL);
+            // console.log("deltaL", deltaL);
 
 
             // the delta for chroma
             var deltaC = Lab2.C_1 - Lab1.C_1;
-            console.log("deltaC", deltaC);
+            // console.log("deltaC", deltaC);
 
 
             // Calculate CIEDE2000 Color-Difference 
@@ -272,8 +272,8 @@ var colorLab = (function(space, values){
             var L_ave = (Lab1.L + Lab2.L) / 2;
             var C_1ave = (Lab1.C_1 + Lab2.C_1) / 2;
 
-            console.log("L_ave", L_ave);
-            console.log("C_1ave", C_1ave);
+            // console.log("L_ave", L_ave);
+            // console.log("C_1ave", C_1ave);
 
             var hDiff;
             if ( ( Lab1.C_1 * Lab2.C_1 ) === 0 ) {
@@ -291,35 +291,35 @@ var colorLab = (function(space, values){
                hDiff = hDiff / 2;
             }
 
-            console.log("hDiff", hDiff);
+            // console.log("hDiff", hDiff);
 
 
             var L_aveMinus50pow2 = Math.pow((L_ave - 50), 2);
-            console.log("L_aveMinus50pow2", L_aveMinus50pow2);
+            // console.log("L_aveMinus50pow2", L_aveMinus50pow2);
 
 
             var SL = 1 + ((0.015 * L_aveMinus50pow2) / Math.sqrt(20 + L_aveMinus50pow2));
-            console.log("SL", SL);
+            // console.log("SL", SL);
 
             var SC = 1 + 0.045 * C_1ave;
-            console.log("SC", SC);
+            // console.log("SC", SC);
 
 
             var T = 1 - 0.17 * Math.cos( root.helper.math.toRad( hDiff - 30 ) ) + 0.24 * Math.cos( root.helper.math.toRad( 2 * hDiff ) ) + 0.32 * Math.cos( root.helper.math.toRad( 3 * hDiff + 6 ) ) - 0.20 * Math.cos( root.helper.math.toRad( 4 * hDiff - 63 ) );
 
-            console.log("T", T);
+            // console.log("T", T);
 
             var SH = 1 + 0.015 * C_1ave * T;
-            console.log("SH", SH);
+            // console.log("SH", SH);
 
             var dTheta = 30 * Math.exp(-1 * Math.pow((hDiff - 275 ) / 25, 2));
-            console.log("dTheta", dTheta);
+            // console.log("dTheta", dTheta);
 
             var RC = 2 * Math.sqrt(Math.pow(C_1ave, 7) / (Math.pow(C_1ave, 7) + Math.pow(25, 7)));
-            console.log("RC", RC);
+            // console.log("RC", RC);
 
             var RT = 0 - Math.sin(root.helper.math.toRad( 2 * dTheta)) * RC;
-            console.log("RT", RT);
+            // console.log("RT", RT);
 
 
 
@@ -327,13 +327,13 @@ var colorLab = (function(space, values){
             var dkC = deltaC / (k_C * SC);
             var dkH = deltaH / (k_H * SH);
 
-            console.log("dkL", dkL);
-            console.log("dkC", dkC);
-            console.log("dkH", dkH);
+            // console.log("dkL", dkL);
+            // console.log("dkC", dkC);
+            // console.log("dkH", dkH);
 
             var CIEDE2000 = Math.sqrt(Math.pow(dkL, 2) + Math.pow(dkC, 2) + Math.pow(dkH, 2) + RT * dkC * dkH );
 
-            console.log("CIEDE2000", CIEDE2000);
+            // console.log("CIEDE2000", CIEDE2000);
 
             return CIEDE2000;
 
