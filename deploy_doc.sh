c16c7954b72f7c047bba5938566a3a5ce88eb5e3
signalwerk <sh@signalwerk.ch>
2017-05-15 03:10:13 +0200

    MOD: debug deploy

--- deploy_doc.sh
+++ deploy_doc.sh
@@ -43,9 +43,11 @@ cd $DEPLOY_DIR
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
+echo "-- DEBUG --"
+find ./..
+
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
 if git diff --quiet; then
-    find ./..
     echo "No changes to the output on this push; exiting."
     exit 0
 fi
