4745cb064b01b55a3022ce3926dfe29692a80ecd
signalwerk <sh@signalwerk.ch>
2017-05-15 02:56:23 +0200

    MOD: debug for gh-pages

--- deploy_doc.sh
+++ deploy_doc.sh
@@ -45,6 +45,7 @@ git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
 if git diff --quiet; then
+    ls -las
     echo "No changes to the output on this push; exiting."
     exit 0
 fi
