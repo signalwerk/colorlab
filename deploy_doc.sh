75b4bdf389a622a4db412ccf918910e8e146bd26
signalwerk <sh@signalwerk.ch>
2017-05-15 04:01:17 +0200

    FIX: deploy script

--- deploy_doc.sh
+++ deploy_doc.sh
@@ -12,10 +12,10 @@ DEPLOY_DIR="gh-pages"
 function doCompile {
   ## npm test
   npm run build
-  mkdir -p './gh-pages/lib/colorlab/'
-  cp  -a './dist/' './gh-pages/lib/colorlab/'
-  cp -a './example/.' './gh-pages/'
-  sed -i'.bak' 's$../dist/colorlab.js$./lib/colorlab/colorlab.min.js$g' './gh-pages/index.html'
+  mkdir -p ./gh-pages/lib/colorlab/
+  cp ./dist/* ./gh-pages/lib/colorlab/
+  cp ./example/*.* ./gh-pages/
+  sed -i'.bak' 's$../dist/colorlab.js$./lib/colorlab/colorlab.min.js$g' ./gh-pages/index.html
   rm -f ./gh-pages/*.bak
 }
 
