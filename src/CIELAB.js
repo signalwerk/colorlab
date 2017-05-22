a23fb1e41aaa57c522aaabc0af39b32df4d40423
signalwerk <sh@signalwerk.ch>
2017-05-22 17:46:46 +0200

    FIX: codestyle

--- src/CIELAB.js
+++ src/CIELAB.js
@@ -10,7 +10,7 @@ class CIELAB {
   }
 
   toString() {
-    return 'L: ' + this.L + ', a: ' + this.a + ', b: ' + this.b;
+    return `L: ${this.L}, a: ${this.a}, b: ${this.b}`;
   }
 
   get chroma() {
@@ -23,25 +23,25 @@ class CIELAB {
 
   set L(L) {
     this._L = clamp(L, 0, 100);
-  };
+  }
 
   get L() {
     return this._L;
-  };
+  }
 
   set a(a) {
     this._a = clamp(a, -128, 128);
-  };
+  }
   get a() {
     return this._a;
-  };
+  }
 
   set b(b) {
     this._b = clamp(b, -128, 128);
-  };
+  }
   get b() {
     return this._b;
-  };
+  }
 }
 
 export default CIELAB;
