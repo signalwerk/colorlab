5f00ca83f78e00a24889071f25abf5c67a3b4b50
signalwerk <sh@signalwerk.ch>
2017-05-22 17:42:36 +0200

    MOD: remove unused functions

--- src/helper.js
+++ src/helper.js
@@ -7,17 +7,13 @@ export const toRad = (num) => {
   return num * Math.PI / 180;
 }
 
-// Convert from 0-1 to 0-255
-export const to8bit = (num) => {
-  return root.helper.math.range8bit(num * 255);
-}
-
-// Convert from 0-1 to 0-255
-export const range8bit = (num) => {
-  return Math.round(Math.min(255, Math.max(0, num)));
-}
-
 // get chroma from (L)ab
 export const chroma = (a, b) => {
   return Math.sqrt(a ** 2 + b ** 2);
+}
+
+//  ECMAScript 2017
+// Math.clamp(x, lower, upper)
+export const clamp = (number, min, max) => {
+  return Math.max(min, Math.min(number, max));
 }
